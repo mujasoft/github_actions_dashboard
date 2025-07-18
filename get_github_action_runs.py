4d3624bdaf084fa23ac572255c5186976c842a62
mujaheedkhan <mujaheedkhan07@gmail.com>
2025-07-18 01:51:23 -0400

    WIP

--- get_github_action_runs.py
+++ get_github_action_runs.py
@@ -104,7 +104,6 @@ def get_github_actions_run(
     # Dump settings.
     max_no_of_pages = settings.get("max_no_of_pages")
     repo = settings.get("repo")
-    print(repo)
     no_of_results_per_page = settings.get("no_of_results_per_page")
     github_api_url = settings.get("github_api_url")
 
